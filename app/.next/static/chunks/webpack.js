50b82b012cee35b6b6a253cc36b2cf31c4bd1ed8
Jitendra Sharma <jks0586@gmail.com>
2023-02-22 03:30:28 +0530

    update

--- app/.next/static/chunks/webpack.js
+++ app/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d89b450c0ca720bf"; }
+/******/ 		__webpack_require__.h = function() { return "29abcfebc1f5169b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
